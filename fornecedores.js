d26cf96a1a08e5c7587abf0f84086a53e208f610
PedroRibeiro08 <pedro.rib.0802@gmail.com>
2026-06-01 20:16:14 -0300

    Update fornecedores.js

--- fornecedores.js
+++ fornecedores.js
@@ -1,66 +1,39 @@
-$(document).ready(function () {
+$("#btnPesquisar").click(function () {
 
-    const ref = db.ref("fornecedores");
+    let termo = $("#pesquisa").val().toLowerCase();
 
-    $("#salvar").click(function () {
+    $("#lista").html("");
 
-        let nome = $("#nome").val();
-        let cnpj = $("#cnpj").val();
-        let email = $("#email").val();
-        let estado = $("input[name='estado']:checked").val();
+    ref.once("value", function (snapshot) {
 
-        if (nome === "" || cnpj === "" || email === "" || !estado) {
-            alert('Preencha todos os campos');
-            return;
-        }
+        snapshot.forEach(function (item) {
 
-        ref.push({ nome, cnpj, email, estado });
+            let chave = item.key;
+            let fornecedor = item.val();
 
-        limpar();
+            if (
+                fornecedor.nome.toLowerCase().includes(termo) ||
+                fornecedor.email.toLowerCase().includes(termo)
+            ) {
+
+                $("#lista").append(`
+                    <tr>
+                        <td>${fornecedor.nome}</td>
+                        <td>${fornecedor.cnpj}</td>
+                        <td>${fornecedor.email}</td>
+                        <td>${fornecedor.estado}</td>
+                        <td>
+                            <button class="btn btn-danger excluir"
+                                data-id="${chave}">
+                                Excluir
+                            </button>
+                        </td>
+                    </tr>
+                `);
+            }
+
+        });
+
     });
-    
-    ref.on("value", dados_tabela => {
-    $("#lista").empty();
-
-    $("#lista").append(`
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th colspan="2">Opções</th>
-        </tr>
-        `);
-
-    dados_tabela.forEach(registro => {
-        let reg = registro.val();
-        let id = registro.key;
-
-        $("#lista").append(`
-            <tr>
-                <td>${id}</td>
-                <td>${reg.nome}</td>
-                <td>${reg.email}</td>
-                <td>
-                    <button class="btn btn-outline-danger btn-sm">
-                        <i class="bi bi-trash"></i>
-                    </button>
-                </td>
-                <td>
-                    <button class="btn btn-outline-warning btn-sm">        
-                        <i class="bi bi-pencil"></i>
-                    </button>
-                </td>
-            </tr>
-            `);
-    });
-});
-
-    function limpar() {
-        $("#nome").val("");
-        $("#cnpj").val("");
-        $("#email").val("");
-        $("input[name='estado']").prop("checked", false);
-        $("#nome").focus();
-    }
 
 });
